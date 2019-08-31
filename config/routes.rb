ef9293c8e096e4b6b1647f78e6e21cefc801f59e
Cqstanq <marron0116m@gmail.com>
2019-08-31 16:50:56 +0900

    edit group

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources groups, only: [:new, :create, :edit, :update] do
     resources :chats, only: [:index, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
+  end
+end
