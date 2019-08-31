f80b0d63dde863dfee2da54e958f49e60f6844d1
Cqstanq <marron0116m@gmail.com>
2019-08-31 18:51:23 +0900

    fix1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
-  
+
   def index
   end
   
   def new
     @group = Group.new
-    @group.user << current_user
+    @group.users << current_user
   end
 
   def create
@@ -16,13 +16,14 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+  end  
 
   def edit
   end
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_chats_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
@@ -31,7 +32,7 @@ class GroupsController < ApplicationController
   private
   
   def group_params
-    params.require(:group).permit(:new, { :user_ids => []})
+    params.require(:group).permit(:name, { :user_ids => []})
   end
 
   def set_group
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "chats#index"
+  root 'groups#index'
+  get 'groups', to: 'groups#index'
   resources :users, only: [:edit, :update]
-  resources groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update] do
     resources :chats, only: [:index, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
   end
 end
