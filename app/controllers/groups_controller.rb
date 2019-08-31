3a195f49c4fb79db0e88a96d3ef8ceb3fac45f04
Cqstanq <marron0116m@gmail.com>
2019-08-31 16:12:08 +0900

    create group and side_bar group view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,24 @@
 class GroupsController < ApplicationController
+  def index
+  end
+  
   def new
+    @group = Group.new
+    @group.user << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+
+    private
+    def group_params
+      params.require(:group).permit(:new, { :user_ids => []})
+    end
   end
 
   def edit
