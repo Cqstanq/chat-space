2750c04016244d14b555bdb39e533800c550a32d
Cqstanq <marron0116m@gmail.com>
2019-09-05 21:48:47 +0900

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,8 +20,9 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+#Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
+  #〜省略〜
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -33,11 +34,18 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  RSpec.configure do |config|
+    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include ControllerMacros, type: :controller
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
