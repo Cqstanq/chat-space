994a4cb1527a35f6f97621ef9ff00cbb2eb0c0f6
Cqstanq <marron0116m@gmail.com>
2019-09-10 09:12:18 +0900

    Ajax of message

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -1,4 +1,16 @@
 $(function(){
+  function buildChat(chat){
+    var html = `.upper-message
+                  .upper-message__user-name
+                    = ${chat.user_name}
+                  .upper-message__date
+                    = ${chat.created_at}
+                .lower-message
+                  - if ${chat.content}.present?
+                    %p.lower-message__content
+                      = ${chat.content}
+                    = image_tag ${chat.image.url}, class: 'lower-message__image' if chat.image.present?`
+  }  
   $('#new_chat').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +22,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(chat){
+      var html = buildChat(chat);
+      $('.messages').append(html)
+      $('#chat_content').val('')
+    })
+    .fail(function(){
+      alert('メッセージが送信されませんでした')
+    })
+    .always(function(){
+      $(".form__submit").removeAttr("disabled");
     });
-  });
-});
+  })
+})
---
+++ app/views/chats/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.content @chat.content
+json.image.url @chat.image.url
+json.created_at @chat.created_at
+json.user_name @chat.user_name
