d583b96519c1b65c20a6ec87ec25d8b532abf103
Cqstanq <marron0116m@gmail.com>
2019-09-11 21:16:18 +0900

    fix

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -1,21 +1,28 @@
 $(function(){
   function buildChat(chat){
-    var html = `<div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${chat.user_name}
+    var image = chat.image;
+    var content = chat.content;
+    var result_image = (image ? image : '');
+    var result_content = (content ? content : '')
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${chat.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${chat.created_at}
+                    </div>
                   </div>
-                  <div class="upper-message__date">
-                    ${chat.created_at}
-                  </div>
-                </div>
-                <div class="lower-message">
-                  <div class="lower-message__content">
-                    ${chat.content}
-                  </div>
-                  <div class='lower-message__image'>
-                      ${chat.image}
+                  <div class="lower-message">
+                    <div class="lower-message__content">
+                      ${result_content}
+                    </div>
+                    <div class='lower-message__image'>
+                      ${result_image}
+                    </div>
                   </div>
                 </div>`
+    return html    
   }
   $('#new_chat').on('submit', function(e){
     e.preventDefault();
@@ -33,9 +40,8 @@ $(function(){
       var html = buildChat(chat);
       $('.messages').append(html);
       $('#chat_content').val('');
-      $('.form__submit').removeAttr('data-disable-with');
-      $('.messages').animate({ scrollTop: $(html)[0].scrollHeight});
-      return false
+      $(".messages").animate({scrollTop: 10000000000})
+      return false;
     })
     .fail(function(){
       alert('メッセージが送信されませんでした');
@@ -43,6 +49,5 @@ $(function(){
     .always(function(){
       $(".form__submit").removeAttr("disabled");
     })
-
   })
 })
