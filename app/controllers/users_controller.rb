6b6c8f5d16924e8cdf39af670a1e47273f72eb73
Cqstanq <marron0116m@gmail.com>
2019-09-14 06:35:35 +0900

    一気にやってしまった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html 
+      format.json
+    end
+  end
+
   def edit
     @chat = Chat.new
-    @chats = 
   end
 
   def update
